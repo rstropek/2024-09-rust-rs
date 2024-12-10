6cf75bcd6fdd7c6c4ee527f007a06a78ca0c4521
Rainer <rainer@software-architects.at>
2024-12-10 09:44:46 +0100

    Enhance lifetime sample

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -13,6 +13,42 @@ struct Order<'a> {
     quantity: u32,
 }
 
+#[derive(Debug, Copy, Clone)]
+struct Line {
+    start: (f32, f32),
+    end: (f32, f32),
+}
+
+impl Line {
+    fn length(&self) -> f32 {
+        let dx = self.end.0 - self.start.0;
+        let dy = self.end.1 - self.start.1;
+        (dx * dx + dy * dy).sqrt()
+    }
+}
+
+fn get_longer<'a>(line1: &'a Line, line2: &'a Line) -> &'a Line {
+    if line1.length() > line2.length() {
+        line1
+    } else {
+        line2
+    }
+}
+
+fn get_longer_2<'a, 'b>(l1: &'a Line, l2: &'a Line, l3: &'b Line, l4: &'b Line) -> (&'a Line, &'b Line) {
+    let longer1 = get_longer(l1, l2);
+    let longer2 = get_longer(l3, l4);
+    (longer1, longer2)
+}
+
+fn get_longer_str(s1: &'static str, s2: &'static str) -> &'static str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
 fn main() {
     {
         let customer = Customer {
@@ -62,4 +98,25 @@ fn main() {
 
         customers.remove(0);
     }
+
+    let longer;
+    let line1 = Line {
+        start: (0.0, 0.0),
+        end: (1.0, 1.0),
+    };
+    {
+        let line2 = Line {
+            start: (0.0, 0.0),
+            end: (2.0, 2.0),
+        };
+        longer = get_longer(&line1, &line2);
+        println!("Longer line: {:?}", longer);
+    }
+
+    println!("Line1: {:?}", line1);
+
+    let s1 = "Hello";
+    let s2 = "World";
+    let longer = get_longer_str(s1, s2);
+    println!("Longer string: {}", longer);
 }
